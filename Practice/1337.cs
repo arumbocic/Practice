3f158a59d067576ea02c570850c97e52db49cce9
Ivan <ivan.vic.os@gmail.com>
2022-03-18 13:02:00 +0100

    text1

---
+++ Practice/1337.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class Klasa1337xd
+    {
+    }
+}
